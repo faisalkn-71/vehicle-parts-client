91575b93462a66ef1d107f69e4a146ea8d1eb459
faisalkn-71 <csust2021@gmail.com>
2022-05-24 15:33:51 +0600

    header-done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Header from './Pages/Shared/Header';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Blogs from './Pages/Blogs/Blogs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-12">
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='login' element={<Login></Login>}></Route>
+      </Routes>
+      
+
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        motortheme: {
+          primary: "#0ea5e9",
+          secondary: "#2563eb",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
